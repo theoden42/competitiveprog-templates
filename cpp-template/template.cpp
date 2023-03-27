0975b218e416ba4dd545fa0252e62246ce4614fb
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-03-28 01:36:06 +0530

    Update template and add RMQ snippet

--- cpp-template/template.cpp
+++ cpp-template/template.cpp
@@ -1,26 +1,34 @@
 
-/* @uthor: (g)theoden42 */
+/* author: (g)theoden42 */
 
 #include <bits/stdc++.h>
-using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(a) a.begin(), a.end()
 #ifdef ON_PC
     #include <debug.h>
 #else
     #define debug(x...)
 #endif
 
-#define ll long long
-#define ld long double
-#define all(a) (a).begin(), (a).end()
+template<typename T>
+using ordset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll =  long long;
+using ld = long double;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int MAX_N = 1e6 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
 void solve() {
-    
+
 }
+
 
 int main() {
     ios_base::sync_with_stdio(0);
