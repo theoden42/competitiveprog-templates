1adec37281c867f79b182ccb551477daaedeb507
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-09-05 17:46:49 +0530

    fix dsu implementation

--- algorithms/DSU.cpp
+++ algorithms/DSU.cpp
@@ -1,34 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
 struct dsu{
 	vector<int> parent;
 	vector<int> sz;
 
 	void init(int n){
 		parent.resize(n);
-		size.resize(n);
+		sz.resize(n);
 		for(int i = 0; i < n; i++){
 			parent[i] = i;
-			size[i] = 1;
+			sz[i] = 1;
 		}
 	}
 
 	int getparent(int x){
-		return parent[x] = (parent[x] == x ? x : getparent(x)); 
+		return parent[x] = (parent[x] == x ? x : getparent(parent[x])); 
 	}
 
 	int unite(int a, int b){
 		a = getparent(a);
 		b = getparent(b);
-		if(a == b)return;
-
+		if(a == b) 
+			return 0;
 		if(sz[a] < sz[b])
 			swap(a, b);
-
 		parent[b] = a;
 		sz[a] += sz[b];
+		return 1;
 	}
-
-}
+};
